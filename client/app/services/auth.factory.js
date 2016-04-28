69206fc85fd76f02a8475a3d9a97b3b0ea34e830
Tiffany Wu <tiffanymwu@gmail.com>
2016-04-27 20:43:27 -0700

    [progress] signup

--- client/app/services/auth.factory.js
+++ client/app/services/auth.factory.js
@@ -1,1 +1,55 @@
-auth.factory.js
+(function () {
+  'use strict'
+
+  angular
+    .module('baby')
+    .factory('Auth', Auth);
+
+  function Auth ($http, $localStorage, $state) {
+    return {
+      current: function () {
+        return {
+          username: $localStorage.username,
+          token: $localStorage.token
+        }
+      },
+
+      signIn: function (username, password) {
+        $http.post(AWS_URL + '/landing/login', {
+          user: {
+            username: username,
+            password: password
+          }
+        }).success(function (data) {
+          $localStorage.username = username
+          $localStorage.token = data.token
+          $http.defaults.headers.common['username'] = username
+          $http.defaults.headers.common['token'] = data.token
+          $state.go('tab.classrooms')
+        })
+      },
+
+      signUp: function (username, password, email, name) {
+        $http.post(AWS_URL + '/landing/register', {
+          user: {
+            username: username,
+            password: password,
+            email: email,
+            name: name
+          }
+        }).success(function (data) {
+          $localStorage.username = username
+          $localStorage.token = data.token
+          $http.defaults.headers.common['username'] = username
+          $http.defaults.headers.common['token'] = data.token
+          $state.go('dashboard')
+        })
+      },
+
+      signOut: function () {
+        $localStorage.$reset()
+        $state.go('landing')
+      };
+    };
+  };
+})();
--- client/app/signup/signup.routes.js
+++ client/app/signup/signup.routes.js
@@ -1,1 +1,20 @@
-signup.routes.js
+(function () {
+  'use strict'
+
+  angular
+    .module('baby.signup')
+    .config(config);
+
+  function config ($stateProvider) {
+    $stateProvider
+      .state('signup', {
+        url: '/signup',
+        views: {
+          'signup': {
+            templateUrl: 'app/signup/signup.html',
+            controller: 'SignupController as signup'
+          };
+        };
+      });
+  };
+})();
