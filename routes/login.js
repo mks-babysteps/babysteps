a675a7548a02889c9c733bf4407887e05b4ff32c
Danny Thai <dthai92@yahoo.com>
2016-05-02 13:37:45 -0700

    [create] login routing (incomplete)

--- routes/login.js
+++ routes/login.js
@@ -1,50 +1,12 @@
 var express = require('express');
 var router = express.Router();
-// var auth = require('../tokens.js');
+var mongoose = require('mongoose');
 
-// var User = require('../db');
+router.get('/', function(req, res) {
+  var username = req.headers.username;
+  var password = req.headers.password;
 
-router.post('/', function(req, res) {
-  var user = req.body;
 
-  // data validation
-  if (!validate(user)) {
-    res.json({
-      success: false,
-      message: 'Please provide a username and password.'
-    });
+})
 
-  // if data validation passes
-  // Danny, need to change to mongo query 
-  } else {
-    // knex('users').where({username: user.username})
-    //   .then(function(resp) {
-    //     var userFromDb = resp[0];
-
-    //     if (userFromDb && userFromDb.password === user.password) {
-    //       res.json({
-    //         success: true,
-    //         message: 'Logged in!',
-    //         token: auth.genToken(userFromDb)
-    //       });
-    //     } else {
-    //       res.json({
-    //         success: false,
-    //         message: 'Username or password is incorrect.'
-    //       });
-    //     }
-    // });
-  }
-});
-
-// router.get('/', function(req, res) {
-  
-// });
-
-// helper function
-function validate(user) {
-  return user.username && user.password;
-}
-
-// export router
 module.exports = router;
