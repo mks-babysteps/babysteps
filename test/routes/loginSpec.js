1b7f1d132bf7c3182c0624865eb17d1d923622e9
Danny Thai <dthai92@yahoo.com>
2016-05-03 09:17:27 -0700

    [update] add tests for login route

--- test/routes/loginSpec.js
+++ test/routes/loginSpec.js
@@ -2,7 +2,47 @@ supertest = require('supertest');
 request = supertest('http://localhost:8080');
 
 describe('Login Routes', function() {
-  it('should retrieve username and password for an existing user', function(done) {
+
+  it('should fail when given a username that does not exist', function(done) {
+    request.get('/login')
+      .set('Accept', 'application/json')
+      .set('username', 'Zimbabwe')
+      .set('password', 'tiff')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        expect(res.body.success).toBe(false);
+        done();
+      });
+  });
+
+  it('should fail when not given a username', function(done) {
+    request.get('/login')
+      .set('Accept', 'application/json')
+      .set('username', '')
+      .set('password', 'tiff')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        expect(res.body.success).toBe(false);
+        done();
+      });
+  });
+
+  it('should fail when not given a password', function(done) {
+    request.get('/login')
+      .set('Accept', 'application/json')
+      .set('username', 'tiff')
+      .set('password', '')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        expect(res.body.success).toBe(false);
+        done();
+      });
+  });
+
+  it('should respond with a success given a correct username and password', function(done) {
     request.get('/login')
       .set('Accept', 'application/json')
       .set('username', 'tiff')
@@ -14,6 +54,7 @@ describe('Login Routes', function() {
         done();
       });
   });
+
 });
 
 
