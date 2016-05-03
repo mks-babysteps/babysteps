42572df6eaf632289ee5e647fccca315a79863ca
prexsa <prexgit@gmail.com>
2016-05-03 12:01:51 -0700

    [progress] add child to db

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -5,6 +5,7 @@
     .module('baby', [
       /* Shared Modules */
       'ui.router',
+      'ui.bootstrap',
       /* Feature areas */
       'baby.landing',
       'baby.signup',
--- client/app/services/dashboard.factory.js
+++ client/app/services/dashboard.factory.js
@@ -7,18 +7,21 @@
 
     function dashboard($http){
       var service = {
-        getUser: getUser
+        getUser: getUser,
+        addChild: addChild
       };
       return service;
-    
+
       function getUser(){
         return $http.get('/dashboard');
       }
+
+      function addChild(params){
+        return $http.post('/dashboard/addChild', params)
+          .success(function(resp){
+            console.log('resp', resp);
+            console.log('resp.success', resp.success);
+          })
+      }
     }
-
-
-
-
-
-
 })();
