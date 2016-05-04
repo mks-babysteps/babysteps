996dbc1ea829b09d43de2cdfca4771def717d640
Danny Thai <dthai92@yahoo.com>
2016-05-03 18:07:38 -0700

    [update] implement ui-bootstrap dependency

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
       'node_modules/angular-mocks/angular-mocks.js',
       'node_modules/angular-ui-router/release/angular-ui-router.js',
       'node_modules/ngstorage/ngStorage.js',
+      'node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js',
       'client/app/signup/signup.module.js',
       'client/app/landing/landing.module.js',
       'client/app/dashboard/dashboard.module.js',
