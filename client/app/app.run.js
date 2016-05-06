0ee65979f931e8e09f11595a9ba84f963ba23e7f
Danny Thai <dthai92@yahoo.com>
2016-05-06 09:01:47 -0700

    [fix] token sent to server throughout app

--- client/app/app.run.js
+++ client/app/app.run.js
@@ -6,15 +6,10 @@
     .run(run);
 
   function run($http, $state, auth, $rootScope, $localStorage) {
-    // if (auth.current().token) {
-    //   // appends authentication information to each http request
-    //   $http.defaults.headers.common.username = auth.current().username;
-    //   $http.defaults.headers.common.token = auth.current().token;
-
-    //   $state.go('dashboard');
-    // } else {
-    //   $state.go('landing');
-    // }
+    if (auth.current().token) {
+      $http.defaults.headers.common.username = auth.current().username;
+      $http.defaults.headers.common.token = auth.current().token;
+    }
     $rootScope.$on('$stateChangeStart', function(event, toState) {
       var requiredLogin = toState.data.requiredLogin;
 
