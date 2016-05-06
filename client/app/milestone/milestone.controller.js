353441434f015fe79bbfeb56ef7715833ae585bf
eshinx1 <eshinx1@gmail.com>
2016-05-06 08:36:33 -0700

    [progress] chart added

--- client/app/milestone/milestone.controller.js
+++ client/app/milestone/milestone.controller.js
@@ -25,296 +25,101 @@
       milestone.getCondition(currentChildCondition)
         .then(function(data) {
           var conditionData = data.data[0];
-          vm.grossMotor = conditionData.grossMotor;
-          vm.personalSocial = conditionData.personalSocial;
-          vm.language = conditionData.language;
+          console.log(conditionData);
+          // vm.grossMotor = conditionData.grossMotor;
+          // vm.personalSocial = conditionData.personalSocial;
+          // vm.language = conditionData.language;
+
+          AmCharts.useUTC = true;
+          AmCharts.makeChart( 'chartdiv', {
+            'type': 'serial',
+            'categoryField': 'category',
+            'rotate': true,
+            'startDuration': 1,
+            'theme': 'light',
+            'categoryAxis': {
+              'gridPosition': 'start'
+            },
+            'trendLines': [],
+            'graphs': [ {
+              'balloonText': 'Month Start:[[open]] Month End:[[close]]',
+              'closeField': 'Month End',
+              'fillAlphas': 1,
+              'id': 'AmGraph-1',
+              'openField': 'Month Start',
+              'title': 'graph 1',
+              'type': 'column'
+            } ],
+            'guides': [],
+            'valueAxes': [ {
+              'id': 'ValueAxis-1',
+              'stackType': 'regular',
+              'title': 'Axis title'
+            } ],
+            'allLabels': [],
+            'balloon': {},
+            'titles': [ {
+              'id': 'Title-1',
+              'size': 15,
+              'text': 'Chart Title'
+            } ],
+            'dataProvider': [ {
+              'category': 'Walks Alone',
+              'Month Start': conditionData.grossMotor.walksalone[0],
+              'Month End': conditionData.grossMotor.walksalone[1]
+              },
+              {
+              'category': 'Stands Alone',
+              'Month Start': conditionData.grossMotor.stands[0],
+              'Month End': conditionData.grossMotor.stands[1]
+              },
+              {
+              'category': 'Crawls',
+              'Month Start': conditionData.grossMotor.crawls[0],
+              'Month End': conditionData.grossMotor.crawls[1]
+              },
+              {
+              'category': 'Sits Alone',
+              'Month Start': conditionData.grossMotor.sitsalone[0],
+              'Month End': conditionData.grossMotor.sitsalone[1]
+              },
+              //Personal Social
+              {
+              'category': 'Uses Spoon',
+              'Month Start': conditionData.personalSocial.usesspoon[0],
+              'Month End': conditionData.personalSocial.usesspoon[1]
+              },
+              {
+              'category': 'Drinks Unassisted',
+              'Month Start': conditionData.personalSocial.drinksunassisted[0],
+              'Month End': conditionData.personalSocial.drinksunassisted[1]
+              },
+              {
+              'category': 'Finger Feeds',
+              'Month Start': conditionData.personalSocial.fingerfeeds[0],
+              'Month End': conditionData.personalSocial.fingerfeeds[1]
+              },
+              {
+              'category': 'Responsive Smile',
+              'Month Start': conditionData.personalSocial.responsivesmile[0],
+              'Month End': conditionData.personalSocial.responsivesmile[1]
+              },
+              //Language
+              {
+              'category': 'Two Word Phrases',
+              'Month Start': conditionData.language.twowordphrases[0],
+              'Month End': conditionData.language.twowordphrases[1]
+              },
+              {
+              'category': 'First Words',
+              'Month Start': conditionData.language.firstwords[0],
+              'Month End': conditionData.language.firstwords[1]
+              },
+            ]
+          });
         });
     }
-
-
-
-  AmCharts.useUTC = true;
-var chart = AmCharts.makeChart( "chartdiv", {
-    "type": "gantt",
-    "theme": "light",
-    "marginRight": 70,
-    "period": "hh",
-    "dataDateFormat":"YYYY-MM-DD",
-    "balloonDateFormat": "JJ:NN",
-    "columnWidth": 0.5,
-    "valueAxis": {
-        "type": "date",
-        "minimum": 7,
-        "maximum": 31
-    },
-    "brightnessStep": 10,
-    "graph": {
-        "fillAlphas": 1,
-        "balloonText": "<b>[[task]]</b>: [[open]] [[value]]"
-    },
-    "rotate": true,
-    "categoryField": "category",
-    "segmentsField": "segments",
-    "colorField": "color",
-    "startDate": "2015-01-01",
-    "startField": "start",
-    "endField": "end",
-    "durationField": "duration",
-    "dataProvider": [ {
-        "category": "John",
-        "segments": [ {
-            "start": 7,
-            "duration": 2,
-            "color": "#46615e",
-            "task": "Task #1"
-        }, {
-            "duration": 2,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "duration": 2,
-            "color": "#8dc49f",
-            "task": "Task #3"
-        } ]
-    }, {
-        "category": "Smith",
-        "segments": [ {
-            "start": 10,
-            "duration": 2,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "duration": 1,
-            "color": "#8dc49f",
-            "task": "Task #3"
-        }, {
-            "duration": 4,
-            "color": "#46615e",
-            "task": "Task #1"
-        } ]
-    }, {
-        "category": "Ben",
-        "segments": [ {
-            "start": 12,
-            "duration": 2,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "start": 16,
-            "duration": 2,
-            "color": "#FFE4C4",
-            "task": "Task #4"
-        } ]
-    }, {
-        "category": "Mike",
-        "segments": [ {
-            "start": 9,
-            "duration": 6,
-            "color": "#46615e",
-            "task": "Task #1"
-        }, {
-            "duration": 4,
-            "color": "#727d6f",
-            "task": "Task #2"
-        } ]
-    }, {
-        "category": "Lenny",
-        "segments": [ {
-            "start": 8,
-            "duration": 1,
-            "color": "#8dc49f",
-            "task": "Task #3"
-        }, {
-            "duration": 4,
-            "color": "#46615e",
-            "task": "Task #1"
-        } ]
-    }, {
-        "category": "Scott",
-        "segments": [ {
-            "start": 15,
-            "duration": 3,
-            "color": "#727d6f",
-            "task": "Task #2"
-        } ]
-    }, {
-        "category": "Julia",
-        "segments": [ {
-            "start": 9,
-            "duration": 2,
-            "color": "#46615e",
-            "task": "Task #1"
-        }, {
-            "duration": 1,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "duration": 8,
-            "color": "#8dc49f",
-            "task": "Task #3"
-        } ]
-    }, {
-        "category": "Bob",
-        "segments": [ {
-            "start": 9,
-            "duration": 8,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "duration": 7,
-            "color": "#8dc49f",
-            "task": "Task #3"
-        } ]
-    }, {
-        "category": "Kendra",
-        "segments": [ {
-            "start": 11,
-            "duration": 8,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "start": 16,
-            "duration": 2,
-            "color": "#FFE4C4",
-            "task": "Task #4"
-        } ]
-    }, {
-        "category": "Tom",
-        "segments": [ {
-            "start": 9,
-            "duration": 4,
-            "color": "#46615e",
-            "task": "Task #1"
-        }, {
-            "duration": 3,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "duration": 5,
-            "color": "#8dc49f",
-            "task": "Task #3"
-        } ]
-    }, {
-        "category": "Kyle",
-        "segments": [ {
-            "start": 6,
-            "duration": 3,
-            "color": "#727d6f",
-            "task": "Task #2"
-        } ]
-    }, {
-        "category": "Anita",
-        "segments": [ {
-            "start": 12,
-            "duration": 2,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "start": 16,
-            "duration": 2,
-            "color": "#FFE4C4",
-            "task": "Task #4"
-        } ]
-    }, {
-        "category": "Jack",
-        "segments": [ {
-            "start": 8,
-            "duration": 10,
-            "color": "#46615e",
-            "task": "Task #1"
-        }, {
-            "duration": 2,
-            "color": "#727d6f",
-            "task": "Task #2"
-        } ]
-    }, {
-        "category": "Kim",
-        "segments": [ {
-            "start": 12,
-            "duration": 2,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "duration": 3,
-            "color": "#8dc49f",
-            "task": "Task #3"
-        } ]
-    }, {
-        "category": "Aaron",
-        "segments": [ {
-            "start": 18,
-            "duration": 2,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "duration": 2,
-            "color": "#FFE4C4",
-            "task": "Task #4"
-        } ]
-    }, {
-        "category": "Alan",
-        "segments": [ {
-            "start": 17,
-            "duration": 2,
-            "color": "#46615e",
-            "task": "Task #1"
-        }, {
-            "duration": 2,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "duration": 2,
-            "color": "#8dc49f",
-            "task": "Task #3"
-        } ]
-    }, {
-        "category": "Ruth",
-        "segments": [ {
-            "start": 13,
-            "duration": 2,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "duration": 1,
-            "color": "#8dc49f",
-            "task": "Task #3"
-        }, {
-            "duration": 4,
-            "color": "#46615e",
-            "task": "Task #1"
-        } ]
-    }, {
-        "category": "Simon",
-        "segments": [ {
-            "start": 10,
-            "duration": 3,
-            "color": "#727d6f",
-            "task": "Task #2"
-        }, {
-            "start": 17,
-            "duration": 4,
-            "color": "#FFE4C4",
-            "task": "Task #4"
-        } ]
-    } ],
-    "valueScrollbar": {
-        "autoGridCount":true
-    },
-    "chartCursor": {
-        "cursorColor":"#55bb76",
-        "valueBalloonsEnabled": false,
-        "cursorAlpha": 0,
-        "valueLineAlpha":0.5,
-        "valueLineBalloonEnabled": true,
-        "valueLineEnabled": true,
-        "zoomable":false,
-        "valueZoomable":true
-    },
-    "export": {
-        "enabled": true
-     }
-} );
   }
 
-
-})();
+}
+)();
