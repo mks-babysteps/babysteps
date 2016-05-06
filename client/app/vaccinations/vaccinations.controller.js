ab0c70af3ad58cd5ee0914b2f87fd948c6361dfe
Daniel Chen <chend2@mail.uc.edu>
2016-05-06 16:04:57 -0700

    [progress] vaccinations pass test

--- client/app/vaccinations/vaccinations.controller.js
+++ client/app/vaccinations/vaccinations.controller.js
@@ -10,20 +10,16 @@
       var vm = this;
 
       //variables
-      var currentChildCondition = $stateParams.condition;
       var vaccinationData = vaccinationData;
       // functions
       vm.displayVaccinations = displayVaccinations;
       vm.condition = $stateParams.condition;
 
       function displayVaccinations(){
-        console.log('inside controller');
-        console.log("condition", vm.condition);
         vaccinations.getVaccinations(vm.condition)
           .then(function(data) {
             vm.vaccinationData = data.data[0].vaccinations;
-            console.log(data.data[0].vaccinations)
-          })
+          });
       }
 
     }
