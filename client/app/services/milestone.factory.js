4011f14a79c809bb81bd654b4593fbc508656d10
Danny Thai <dthai92@yahoo.com>
2016-05-10 15:47:13 -0700

    [update] find condition data based on child

--- client/app/services/milestone.factory.js
+++ client/app/services/milestone.factory.js
@@ -5,19 +5,19 @@
     .module('baby')
     .factory('milestone', milestone);
 
-  // Milestone.$inject = [$http];
-
   function milestone($http) {
     var service = {
-        getCondition: getCondition
+        getConditionBy: getConditionBy
     };
+
     return service;
 
-    function getCondition(condition) {
-      var conditionObj = {
-        conditionName: condition
-      };
-      return $http.post('/milestone', conditionObj);
+    function getConditionBy(child) {
+      return $http.get('/milestone', {
+        headers: {
+          child: child
+        }
+      });
     }
 
   }
