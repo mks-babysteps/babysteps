0d6292b38ba6d86087a2f794644a377652c64086
Danny Thai <dthai92@yahoo.com>
2016-05-12 16:28:57 -0700

    [update] cell color and matrix data

--- client/app/directives/matrix-diagram.js
+++ client/app/directives/matrix-diagram.js
@@ -127,25 +127,125 @@
                       ],
                     "links":
                       [
-                        // { "source":  0,  "target":  1,  "value":  0 },
-                        // { "source":  1,  "target":  2,  "value": 0 },
-                        // { "source":  1,  "target":  13,  "value":  1 },
-                        // { "source":  1,  "target":  14,  "value":  1 },
-                        // { "source":  1,  "target":  15,  "value":  1 },
-                        // { "source":  1,  "target":  16,  "value":  1 },
-                        // { "source":  1,  "target":  17,  "value":  1 },
-                        // { "source":  1,  "target":  18,  "value":  1 },
-                        // { "source":  1,  "target":  19,  "value":  1 },
-                        // { "source":  1,  "target":  20,  "value":  1 },
-                        // { "source":  1,  "target":  21,  "value":  1 },
-                        // { "source":  1,  "target":  22,  "value":  1 },
-                        // { "source":  1,  "target":  23,  "value":  1 },
-                        { "source":  1,  "target":  9,  "value":  1 }
-
-
-                        // { "source":  1,  "target":  2,  "value":  1 },
-                        // { "source":  2,  "target":  2,  "value": 0 }
-                        // { "source":  1,  "target":  3,  "value":  0 }
+                        { "source":  0,  "target":  13,  "value":  1 },
+                        { "source":  0,  "target":  14,  "value":  1 },
+                        { "source":  0,  "target":  15,  "value":  1 },
+                        { "source":  0,  "target":  16,  "value":  1 },
+                        { "source":  0,  "target":  17,  "value":  1 },
+                        { "source":  0,  "target":  18,  "value":  1 },
+                        { "source":  0,  "target":  19,  "value":  1 },
+                        { "source":  0,  "target":  20,  "value":  1 },
+                        { "source":  0,  "target":  21,  "value":  1 },
+                        { "source":  0,  "target":  22,  "value":  1 },
+                        { "source":  0,  "target":  23,  "value":  1 },
+                        { "source":  0,  "target":  24,  "value":  1 },
+                        { "source":  1,  "target":  12,  "value":  1 },
+                        { "source":  1,  "target":  13,  "value":  1 },
+                        { "source":  1,  "target":  14,  "value":  1 },
+                        { "source":  1,  "target":  15,  "value":  1 },
+                        { "source":  1,  "target":  16,  "value":  1 },
+                        { "source":  1,  "target":  17,  "value":  1 },
+                        { "source":  1,  "target":  18,  "value":  1 },
+                        { "source":  1,  "target":  19,  "value":  1 },
+                        { "source":  1,  "target":  20,  "value":  1 },
+                        { "source":  1,  "target":  21,  "value":  1 },
+                        { "source":  1,  "target":  22,  "value":  1 },
+                        { "source":  1,  "target":  23,  "value":  1 },
+                        { "source":  1,  "target":  24,  "value":  1 },
+                        { "source":  2,  "target":  10,  "value":  1 },
+                        { "source":  2,  "target":  11,  "value":  1 },
+                        { "source":  2,  "target":  12,  "value":  1 },
+                        { "source":  2,  "target":  13,  "value":  1 },
+                        { "source":  2,  "target":  14,  "value":  1 },
+                        { "source":  2,  "target":  15,  "value":  1 },
+                        { "source":  2,  "target":  16,  "value":  1 },
+                        { "source":  2,  "target":  17,  "value":  1 },
+                        { "source":  2,  "target":  18,  "value":  1 },
+                        { "source":  2,  "target":  19,  "value":  1 },
+                        { "source":  2,  "target":  20,  "value":  1 },
+                        { "source":  2,  "target":  21,  "value":  1 },
+                        { "source":  2,  "target":  22,  "value":  1 },
+                        { "source":  2,  "target":  23,  "value":  1 },
+                        { "source":  2,  "target":  24,  "value":  1 },
+                        { "source":  3,  "target":  2,  "value":  1 },
+                        { "source":  3,  "target":  3,  "value":  1 },
+                        { "source":  3,  "target":  4,  "value":  1 },
+                        { "source":  3,  "target":  5,  "value":  1 },
+                        { "source":  4,  "target":  23,  "value":  1 },
+                        { "source":  4,  "target":  24,  "value":  1 },
+                        { "source":  5,  "target":  12,  "value":  1 },
+                        { "source":  5,  "target":  13,  "value":  1 },
+                        { "source":  5,  "target":  14,  "value":  1 },
+                        { "source":  5,  "target":  15,  "value":  1 },
+                        { "source":  5,  "target":  16,  "value":  1 },
+                        { "source":  5,  "target":  17,  "value":  1 },
+                        { "source":  5,  "target":  18,  "value":  1 },
+                        { "source":  5,  "target":  19,  "value":  1 },
+                        { "source":  5,  "target":  20,  "value":  1 },
+                        { "source":  5,  "target":  21,  "value":  1 },
+                        { "source":  5,  "target":  22,  "value":  1 },
+                        { "source":  5,  "target":  23,  "value":  1 },
+                        { "source":  5,  "target":  24,  "value":  1 },
+                        { "source":  6,  "target":  12,  "value":  1 },
+                        { "source":  6,  "target":  13,  "value":  1 },
+                        { "source":  6,  "target":  14,  "value":  1 },
+                        { "source":  6,  "target":  15,  "value":  1 },
+                        { "source":  6,  "target":  16,  "value":  1 },
+                        { "source":  6,  "target":  17,  "value":  1 },
+                        { "source":  6,  "target":  18,  "value":  1 },
+                        { "source":  6,  "target":  19,  "value":  1 },
+                        { "source":  6,  "target":  20,  "value":  1 },
+                        { "source":  6,  "target":  21,  "value":  1 },
+                        { "source":  6,  "target":  22,  "value":  1 },
+                        { "source":  6,  "target":  23,  "value":  1 },
+                        { "source":  6,  "target":  24,  "value":  1 },
+                        { "source":  7,  "target":  12,  "value":  1 },
+                        { "source":  7,  "target":  13,  "value":  1 },
+                        { "source":  7,  "target":  14,  "value":  1 },
+                        { "source":  7,  "target":  15,  "value":  1 },
+                        { "source":  7,  "target":  16,  "value":  1 },
+                        { "source":  7,  "target":  17,  "value":  1 },
+                        { "source":  7,  "target":  18,  "value":  1 },
+                        { "source":  7,  "target":  19,  "value":  1 },
+                        { "source":  7,  "target":  20,  "value":  1 },
+                        { "source":  7,  "target":  21,  "value":  1 },
+                        { "source":  7,  "target":  22,  "value":  1 },
+                        { "source":  7,  "target":  23,  "value":  1 },
+                        { "source":  7,  "target":  24,  "value":  1 },
+                        { "source":  8,  "target":  8,  "value":  1 },
+                        { "source":  8,  "target":  9,  "value":  1 },
+                        { "source":  8,  "target":  10,  "value":  1 },
+                        { "source":  8,  "target":  11,  "value":  1 },
+                        { "source":  8,  "target":  12,  "value":  1 },
+                        { "source":  8,  "target":  13,  "value":  1 },
+                        { "source":  8,  "target":  14,  "value":  1 },
+                        { "source":  8,  "target":  15,  "value":  1 },
+                        { "source":  8,  "target":  16,  "value":  1 },
+                        { "source":  8,  "target":  17,  "value":  1 },
+                        { "source":  8,  "target":  18,  "value":  1 },
+                        { "source":  8,  "target":  19,  "value":  1 },
+                        { "source":  8,  "target":  20,  "value":  1 },
+                        { "source":  8,  "target":  21,  "value":  1 },
+                        { "source":  8,  "target":  22,  "value":  1 },
+                        { "source":  8,  "target":  23,  "value":  1 },
+                        { "source":  8,  "target":  24,  "value":  1 },
+                        { "source":  9,  "target":  8,  "value":  1 },
+                        { "source":  9,  "target":  9,  "value":  1 },
+                        { "source":  9,  "target":  10,  "value":  1 },
+                        { "source":  9,  "target":  11,  "value":  1 },
+                        { "source":  9,  "target":  12,  "value":  1 },
+                        { "source":  9,  "target":  13,  "value":  1 },
+                        { "source":  9,  "target":  14,  "value":  1 },
+                        { "source":  9,  "target":  15,  "value":  1 },
+                        { "source":  9,  "target":  16,  "value":  1 },
+                        { "source":  9,  "target":  17,  "value":  1 },
+                        { "source":  9,  "target":  18,  "value":  1 },
+                        { "source":  9,  "target":  19,  "value":  1 },
+                        { "source":  9,  "target":  20,  "value":  1 },
+                        { "source":  9,  "target":  21,  "value":  1 },
+                        { "source":  9,  "target":  22,  "value":  1 },
+                        { "source":  9,  "target":  23,  "value":  1 },
+                        { "source":  9,  "target":  24,  "value":  1 }
                       ],
                     "months": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
                    }
@@ -172,11 +272,16 @@ var svg = d3.select(".diagram").append("svg")
 //d3.json("foo.json", function(miserables) {
   var matrix = [],
       nodes = miserables.nodes,
-      n = nodes.length;
+      numRows = nodes.length;
+      n = miserables.months.length + 1;
   console.log('BEFORE MATRIX: ', matrix);
   // Compute index per node.
+  // i represents the index number for each node
+  // j represents the month
+  // z represents the cell value
   nodes.forEach(function(node, i) {
-    node.index = i;
+    // represents how many rows
+    // node.index = i;
     node.count = 0;
     matrix[i] = d3.range(n).map(function(j) {
       return {x: j, y: i, z: 0};
@@ -215,9 +320,9 @@ var svg = d3.select(".diagram").append("svg")
 
   // Precompute the orders.
   var orders = {
-    name: d3.range(n).sort(function(a, b) { return d3.ascending(nodes[a].name, nodes[b].name); }),
-    count: d3.range(n).sort(function(a, b) { return nodes[b].count - nodes[a].count; }),
-    group: d3.range(n).sort(function(a, b) { return nodes[b].group - nodes[a].group; })
+    name: d3.range(numRows).sort(function(a, b) { return d3.ascending(nodes[a].name, nodes[b].name); }),
+    count: d3.range(numRows).sort(function(a, b) { return nodes[b].count - nodes[a].count; })
+    // group: d3.range(numRows).sort(function(a, b) { return nodes[b].group - nodes[a].group; })
   };
 
   // The default sort order.
@@ -236,7 +341,7 @@ var svg = d3.select(".diagram").append("svg")
         // console.log('rows d: ', d);
         // console.log('x(i): ', x(i));
         // return "translate(0," + x(i) + ")";
-        return "translate(0," + 125 + ")";
+        return "translate(0," + x(i) + ")";
 
       })
       .each(row);
@@ -276,23 +381,34 @@ var svg = d3.select(".diagram").append("svg")
       // .text(function(d, i) { return nodes[i].name; });
       .text(function(d, i) { return d; });
 
-
+  // fill opacity determines actual coloring
   function row(row) {
     var cell = d3.select(this).selectAll(".cell")
         .data(row.filter(function(d) {
           // console.log('d inside row: ', d);
+          console.log('d.z: ', d.z);
           return d.z;
         }))
       .enter().append("rect")
         .attr("class", "cell")
-        .attr("x", function(d) { return y(d.x); })
+        .attr("x", function(d) {
+          console.log('d.x: ', d.x);
+          return y(d.x);
+        })
         // .attr("width", x.rangeBand())
         .attr("width", 21)
         .attr("height", x.rangeBand())
         .style("fill-opacity", function(d) { return z(d.z); })
+
         .style("fill", function(d) {
           // console.log('row d: ', d);
-          return nodes[d.x].group == nodes[d.y].group ? c(nodes[d.x].group) : null;
+          // console.log('nodes d.x: ', nodes[d.x].group);
+          // console.log('nodes d.y: ', nodes[d.y].group);
+          // return nodes[d.x].group == nodes[d.y].group ? c(nodes[d.x].group) : null;
+          // return nodes[d.x].group == nodes[d.y].group ? c(0) : null;
+          // console.log('color: ', c(0));
+          // return nodes[d.x] == nodes[d.y] ? c(0) : null;
+          // return
         })
         .on("mouseover", mouseover)
         .on("mouseout", mouseout);
@@ -315,7 +431,9 @@ var svg = d3.select(".diagram").append("svg")
   });
 
   function order(value) {
+    console.log('running: ', value);
     x.domain(orders[value]);
+    console.log('orders: ', orders[value]);
 
     var t = svg.transition().duration(2500);
 
@@ -331,10 +449,10 @@ var svg = d3.select(".diagram").append("svg")
     //     .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
   }
 
-  // var timeout = setTimeout(function() {
-  //   order("group");
-  //   d3.select("#order").property("selectedIndex", 0).node();
-  // }, 500);
+  var timeout = setTimeout(function() {
+    order("count");
+    d3.select("#order").property("selectedIndex", 1).node();
+  }, 500);
 // });
       }
 
