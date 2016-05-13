da6623aeeccc3235d049e874d1649fb13f058f1f
Daniel Chen <chend2@mail.uc.edu>
2016-05-12 20:14:28 -0700

    [progress] vaccinations checkbox

--- client/app/services/vaccinations.factory.js
+++ client/app/services/vaccinations.factory.js
@@ -7,14 +7,22 @@
 
     function vaccinations($http) {
       var service = {
-          getVaccinations: getVaccinations
+          getVaccinations: getVaccinations,
+          updateDoseStatus: updateDoseStatus
       };
       return service;
 
       function getVaccinations(firstName) {
         console.log('firstName in vac factory', firstName);
         return $http.post('/vaccinations', {firstName: firstName});
+      };
+
+      function updateDoseStatus(vaccinationName, doseNumber, doseStatus, firstName){
+        console.log('the doseNumber is', doseNumber);
+        return $http.post('/vaccinations/updateDose', {vaccinationName: vaccinationName, doseNumber: doseNumber, doseStatus: doseStatus, firstName: firstName })
       }
+
+
     }
 
 })();
