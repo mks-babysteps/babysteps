c656148cdbe0c0dd2c0b6f68e07b50df41c5482a
Tiffany Wu <tiffanymwu@gmail.com>
2016-05-17 12:07:38 -0700

    [layout] chat template

--- client/app/chat/chat.controller.js
+++ client/app/chat/chat.controller.js
@@ -5,17 +5,18 @@
     .module('baby.chat')
     .controller('ChatCtrl', ChatCtrl);
 
-  function ChatCtrl(messages, auth) {
+  function ChatCtrl(messages, auth, dashboard) {
     // initialize
     var vm = this;
 
     // variables
-    // vm.messages = messages;
     vm.username = auth.current().username;
     vm.chat = {string: ''};
     vm.text = '';
     vm.messages = messages;
     vm.addMessage = addMessage;
+    vm.displayUsers = displayUsers;
+    vm.clearInput = clearInput;
 
     // functions
     function addMessage(text) {
@@ -27,5 +28,19 @@
       });
       console.log(vm.messages);
       }
+
+    function displayUsers() {
+      dashboard.getUser()
+        .then(function(data) {
+          var userObj = data.data;
+          vm.users = userObj;
+          vm.children = userObj[0].children;
+          vm.imageUrl = vm.users[0].imageUrl;
+        });
     }
+
+    function clearInput() {
+      
+    }
+  }
 })();
