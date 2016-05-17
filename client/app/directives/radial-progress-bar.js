d4709a512aeb6b1c8e6f612f6bfa8b6bb74b4978
Danny Thai <dthai92@yahoo.com>
2016-05-17 13:02:23 -0700

    [update] remove set time interval

--- client/app/directives/radial-progress-bar.js
+++ client/app/directives/radial-progress-bar.js
@@ -4,80 +4,70 @@
 
     function radialBar($window) {
       function linker(scope, el) {
-        console.log('running radial!');
         var d3 = $window.d3;
         el = el[0];
         var data;
-        var hasProgress = false;
         var start = 0;
         var pastPercentage = start/24;
 
         scope.$watch('data', function(newVal, oldVal) {
-            console.log('newVal: ', newVal);
-            if(newVal !== oldVal) {
+            if (newVal !== oldVal) {
               data = newVal;
               d3.select('svg').remove();
-              render(newVal, hasProgress, start);
+              render(newVal);
               start = newVal;
               pastPercentage = start/24;
-              hasProgress = true;
             }
         });
 
-        function render(count, hasProgress, start) {
-          console.log('i am rendering!');
-          console.log('this scope data: ', scope.data);
-          console.log('pastPercentage: ', pastPercentage);
-          // var $container = $('.chart-container'),
+        function render(count) {
           var fraction = count/24;
           var diagramWidth = document.getElementById('progress').clientWidth;
-          var diagramHeight = document.getElementById('progress').clientHeight;
           var τ = 2 * Math.PI,
             width = diagramWidth,
             height = 25,
             outerRadius = Math.min(width,height)/2,
             innerRadius = (outerRadius/5)*4,
+            min = Math.min(width,height),
             fontSize = (Math.min(width,height)/4);
 
-            var arc = d3.svg.arc()
-                .innerRadius(innerRadius)
-                .outerRadius(outerRadius)
-                .startAngle(0);
-            var svg = d3.select('#progress').append("svg")
-                .attr("width", '100%')
-                .attr("height", '100%')
-                .attr('viewBox','0 0 '+Math.min(width,height) +' '+Math.min(width,height) )
-                .attr('preserveAspectRatio','xMinYMin')
-                .append("g")
-                .attr("transform", "translate(" + Math.min(width,height) / 2 + "," + Math.min(width,height) / 2 + ")");
 
-            console.log('past%: ', pastPercentage);
-            var text = svg.append("text")
-                .text(Math.round(pastPercentage * 100) + '%')
-                .attr("text-anchor", "middle")
-                .style("font-size",fontSize+'px')
-                .attr("dy",fontSize/3)
-                .attr("dx",2);
+          var arc = d3.svg.arc()
+              .innerRadius(innerRadius)
+              .outerRadius(outerRadius)
+              .startAngle(0);
+          var svg = d3.select('#progress').append('svg')
+              .attr('width', '100%')
+              .attr('height', '100%')
+              .attr('viewBox','0 0 '+min +' '+min )
+              .attr('preserveAspectRatio','xMinYMin')
+              .append('g')
+              .attr('transform', 'translate('+min/2+','+min/2+')');
 
-            var background = svg.append("path")
-                .datum({endAngle: τ}) // tell it where to start
-                .style("fill", "#7cc35f")
-                .attr("d", arc);
+          var text = svg.append('text')
+              .text(Math.round(pastPercentage * 100) + '%')
+              .attr('text-anchor', 'middle')
+              .style('font-size',fontSize+'px')
+              .attr('dy',fontSize/3)
+              .attr('dx',2);
 
-            var foreground = svg.append("path")
-                .attr("class", "foreground")
-                .datum({endAngle: pastPercentage *  τ})
-                .style("fill", "#57893e")
-                .attr("d", arc);
+          svg.append('path')
+              .datum({endAngle: τ}) // tell it where to start
+              .style('fill', '#7cc35f')
+              .attr('d', arc);
 
-            setInterval(function() {
-              foreground.transition()
-                  .duration(700)
-                  .call(arcTween, fraction * τ);
-            }, 701)
+          var foreground = svg.append('path')
+              .attr('class', 'foreground')
+              .datum({endAngle: pastPercentage *  τ})
+              .style('fill', '#57893e')
+              .attr('d', arc);
+
+            foreground.transition()
+                .duration(300)
+                .call(arcTween, fraction * τ);
 
           function arcTween(transition, newAngle) {
-            transition.attrTween("d", function(d) {
+            transition.attrTween('d', function(d) {
               var interpolate = d3.interpolate(d.endAngle, newAngle);
               return function(t) {
                   d.endAngle = interpolate(t);
